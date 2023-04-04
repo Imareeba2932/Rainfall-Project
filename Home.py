8e09f4146bd4b784f428b8bf97590c3473ad8dc6
Areeba Shakeel <shakeelareeba02@gmail.com>
2023-04-04 18:14:28 +0530

    rainfall analysis and visualisation project

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ st.image("assets/img.png", use_column_width=True)
 
 st.subheader('ABOUT')
 st.markdown('''
-Rianfall is one of the climatological data which is widely analyzed for a long time. Rainfall is a prime input for various engineering design such as hydraulic structures, sewer and road drainage system, bridges and canals etc.
+Rainfall is one of the climatological data which is widely analyzed for a long time. Rainfall is a prime input for various engineering design such as hydraulic structures, sewer and road drainage system, bridges and canals etc.
 The detailed statistical analysis of rainfall data is essential as it facilitates policy decisions reegarding the cropping pattern, sowing date, construction of roads and providing drinking water to urban and rural areas. Statistical
 analyses are carried out to determine the rainfall event intensity for specific return periods.
 
